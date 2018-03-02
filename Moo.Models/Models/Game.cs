224cb1bf883f93a91479a3ebd6c837a0d2c2f69a
Alex <alexchobanov@gmail.com>
2018-03-02 02:11:30 +0200

    Separated Models in their own asembly

---
+++ Moo.Models/Models/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moo.Models.Models
+{
+    class Game
+    {
+        public int ID { get; set; }
+        public bool HasUserWon { get; set; }
+    }
+}
